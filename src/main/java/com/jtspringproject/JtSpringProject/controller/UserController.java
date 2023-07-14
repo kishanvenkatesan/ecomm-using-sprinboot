3bf0d5f31c78fa37738993eabf6c10dc29e0c240
Kishan <kishanvenkatesan@gmail.com>
2023-07-14 19:08:06 +0530

    upload to GitHub

---
+++ src/main/java/com/jtspringproject/JtSpringProject/controller/UserController.java
@@ -0,0 +1,125 @@
+package com.jtspringproject.JtSpringProject.controller;
+
+import com.jtspringproject.JtSpringProject.models.Cart;
+import com.jtspringproject.JtSpringProject.models.Product;
+import com.jtspringproject.JtSpringProject.models.User;
+
+import java.io.Console;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jtspringproject.JtSpringProject.services.cartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.jtspringproject.JtSpringProject.services.userService;
+import com.jtspringproject.JtSpringProject.services.productService;
+import com.jtspringproject.JtSpringProject.services.cartService;
+
+
+
+@Controller
+public class UserController{
+	
+	@Autowired
+	private userService userService;
+
+	@Autowired
+	private productService productService;
+
+	@GetMapping("/register")
+	public String registerUser()
+	{
+		return "register";
+	}
+
+	@GetMapping("/buy")
+	public String buy()
+	{
+		return "buy";
+	}
+	
+
+	@GetMapping("/")
+	public String userlogin(Model model) {
+		
+		return "userLogin";
+	}
+	@RequestMapping(value = "userloginvalidate", method = RequestMethod.POST)
+	public ModelAndView userlogin( @RequestParam("username") String username, @RequestParam("password") String pass,Model model,HttpServletResponse res) {
+		
+		System.out.println(pass);
+		User u = this.userService.checkLogin(username, pass);
+		System.out.println(u.getUsername());
+		if(u.getUsername().equals(username)) {	
+			
+			res.addCookie(new Cookie("username", u.getUsername()));
+			ModelAndView mView  = new ModelAndView("index");	
+			mView.addObject("user", u);
+			List<Product> products = this.productService.getProducts();
+
+			if (products.isEmpty()) {
+				mView.addObject("msg", "No products are available");
+			} else {
+				mView.addObject("products", products);
+			}
+			return mView;
+
+		}else {
+			ModelAndView mView = new ModelAndView("userLogin");
+			mView.addObject("msg", "Please enter correct email and password");
+			return mView;
+		}
+		
+	}
+	
+	
+	@GetMapping("/user/products")
+	public ModelAndView getproduct() {
+
+		ModelAndView mView = new ModelAndView("uproduct");
+
+		List<Product> products = this.productService.getProducts();
+
+		if(products.isEmpty()) {
+			mView.addObject("msg","No products are available");
+		}else {
+			mView.addObject("products",products);
+		}
+
+		return mView;
+	}
+	@RequestMapping(value = "newuserregister", method = RequestMethod.POST)
+	public String newUseRegister(@ModelAttribute User user)
+	{
+		
+		System.out.println(user.getEmail());
+		user.setRole("ROLE_NORMAL");
+		this.userService.addUser(user);
+		
+		return "redirect:/";
+	}
+	
+	
+
+
+
+//	@GetMapping("carts")
+//	public ModelAndView  getCartDetail()
+//	{
+//		ModelAndView mv= new ModelAndView();
+//		List<Cart>carts = cartService.getCarts();
+//	}
+	  
+}
